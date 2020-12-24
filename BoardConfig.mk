b2f6d7b361826ba6535ef486a49ebb85cb0c458b
alexenferman <alex@alexenferman.com>
2020-12-24 18:08:33 -0500

    Create BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,90 @@
+#
+# Copyright (C) 2020 Alexenferman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Platform
+DEVICE_PATH := device/zte/achill
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := achill
+TARGET_BOARD_PLATFORM := MSM8909
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
+
+# Kernel
+# Inline Kernel Building
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_KERNEL_CONFIG := msm8909-achill_defconfig
+TARGET_KERNEL_SOURCE := kernel/zte/achill
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+BOARD_KERNEL_IMAGE_NAME := zImage
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := achill
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0xf000000
+
+# Recovery LMZA Compression
+# Never Enable This Black Screen and Red LED
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/zte/achill/ramdisk/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+#TARGET_USERIMAGES_USE_EXT4 := true
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
+# TWRP Configuration
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
+DEVICE_RESOLUTION := 854x480
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_INCLUDE_CRYPTO := true
+TW_THEME := portrait_mdpi
