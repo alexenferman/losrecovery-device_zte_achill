e9e8f48f5afc7e4822f8d7241c8196a33899ad17
alexenferman <alex@alexenferman.com>
2020-12-24 20:15:41 -0500

    Lineage -> CM

--- lineage_achill.mk
+++ lineage_achill.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/zte/achill/device.mk)
 
 # Inherit Telephony packages
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := achill
